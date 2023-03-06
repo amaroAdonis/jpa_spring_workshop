bf6d86471ea5339b5816ff123730e30158014e7d
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2023-03-06 20:43:53 -0300

    Project Start!

---
+++ src/test/java/com/jpa_spring_course/jpa_spring/JpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa_spring_course.jpa_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
