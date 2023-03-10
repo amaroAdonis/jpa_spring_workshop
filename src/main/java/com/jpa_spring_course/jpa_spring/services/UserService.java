b866f1bb93c9484dadf488df5d4a1e8fa52cba64
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2023-03-10 11:59:52 -0300

    Exception handling - update

--- src/main/java/com/jpa_spring_course/jpa_spring/services/UserService.java
+++ src/main/java/com/jpa_spring_course/jpa_spring/services/UserService.java
@@ -4,6 +4,7 @@ import com.jpa_spring_course.jpa_spring.entities.User;
 import com.jpa_spring_course.jpa_spring.repositories.UserRepository;
 import com.jpa_spring_course.jpa_spring.services.exceptions.DatabaseException;
 import com.jpa_spring_course.jpa_spring.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity,obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
