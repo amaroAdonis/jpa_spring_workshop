43d0ef6aea7ae12ebbfb60c9fd8653e2cded727d
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2023-03-10 11:15:21 -0300

    Exception handling - findById

--- src/main/java/com/jpa_spring_course/jpa_spring/services/UserService.java
+++ src/main/java/com/jpa_spring_course/jpa_spring/services/UserService.java
@@ -2,6 +2,7 @@ package com.jpa_spring_course.jpa_spring.services;
 
 import com.jpa_spring_course.jpa_spring.entities.User;
 import com.jpa_spring_course.jpa_spring.repositories.UserRepository;
+import com.jpa_spring_course.jpa_spring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/jpa_spring_course/jpa_spring/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.jpa_spring_course.jpa_spring.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found! Id " + id);
+    }
+
+}
